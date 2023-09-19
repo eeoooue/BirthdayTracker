ffd504d696c8e9647f64fd4453b75c610a0aa877
eeoooue <pdmoorhouse@gmail.com>
2023-09-19 23:34:12 +0100

    created alphabetised copy of HomeFeed as a ProfileDirectory page

---
+++ birthdaytracker/lib/profiledirectory.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'birthdaybar.dart';
+import 'birthdayprofile.dart';
+import 'profilestore.dart';
+
+class ProfileDirectory extends StatefulWidget {
+  const ProfileDirectory({super.key});
+
+  @override
+  State<StatefulWidget> createState() {
+    return _ProfileDirectoryState();
+  }
+}
+
+class _ProfileDirectoryState extends State<ProfileDirectory> {
+  final List<BirthdayProfile> profiles =
+      ProfileStore().getAlphabeticalOrdering();
+
+  _ProfileDirectoryState();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Center(
+          child: Text(
+            "BirthdayTracker",
+            style: TextStyle(color: Colors.white),
+          ),
+        ),
+        backgroundColor: Colors.blue,
+      ),
+      bottomNavigationBar: BottomNavigationBar(items: [
+        BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
+        BottomNavigationBarItem(icon: Icon(Icons.people), label: "People"),
+        BottomNavigationBarItem(icon: Icon(Icons.settings), label: "Settings"),
+      ]),
+      body: Column(children: [
+        Container(
+          height: 30,
+          color: Colors.grey,
+        ),
+        Expanded(
+            child: ListView.builder(
+                itemCount: profiles.length,
+                itemBuilder: (context, index) {
+                  return BirthdayProfileBar(profiles[index]);
+                }))
+      ]),
+    );
+  }
+}
