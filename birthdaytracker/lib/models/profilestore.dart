bb98a50bb3be6b64137959cf046fab47d011dcb3
eeoooue <pdmoorhouse@gmail.com>
2023-09-20 09:50:54 +0100

    removed hardcoded BirthdayProfiles

--- birthdaytracker/lib/models/profilestore.dart
+++ birthdaytracker/lib/models/profilestore.dart
@@ -1,30 +1,15 @@
 import 'birthdayprofile.dart';
 
 class ProfileStore {
-  final List<BirthdayProfile> profiles = List.empty(growable: true);
-
-  ProfileStore() {
-    populateProfiles();
-  }
-
-  void populateProfiles() {
-    profiles.add(BirthdayProfile("Sergey Karjakin", 1, 12));
-    profiles.add(BirthdayProfile("Veselin Topalov", 3, 15));
-    profiles.add(BirthdayProfile("Anatoly Karpov", 5, 23));
-    profiles.add(BirthdayProfile("Vladimir Kramnik", 6, 25));
-    profiles.add(BirthdayProfile("Judit Polgar", 7, 23));
-    profiles.add(BirthdayProfile("Fabiano Caruana", 7, 30));
-    profiles.add(BirthdayProfile("Levon Aronian", 10, 6));
-    profiles.add(BirthdayProfile("Mikhail Tal", 11, 9));
-    profiles.add(BirthdayProfile("Jose Capablanca", 11, 19));
-    profiles.add(BirthdayProfile("Magnus Carlsen", 11, 30));
-    profiles.add(BirthdayProfile("Hikaru Nakamura", 12, 9));
+  List<BirthdayProfile> getProfiles() {
+    List<BirthdayProfile> profiles = List.empty(growable: true);
+    return profiles;
   }
 
   List<String> getNames() {
     List<String> names = List.empty(growable: true);
 
-    for (BirthdayProfile profile in profiles) {
+    for (BirthdayProfile profile in getProfiles()) {
       names.add(profile.name);
     }
     names.sort();
@@ -34,7 +19,7 @@ class ProfileStore {
 
   List<BirthdayProfile> getAlphabeticalOrdering() {
     List<BirthdayProfile> alphabetical = List.empty(growable: true);
-    for (BirthdayProfile profile in profiles) {
+    for (BirthdayProfile profile in getProfiles()) {
       alphabetical.add(profile);
     }
 
