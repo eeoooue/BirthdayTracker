f59fe988c6acb278d8d92ad61abebaa80f472ac1
eeoooue <pdmoorhouse@gmail.com>
2023-09-20 10:34:35 +0100

    fleshed out first attempt at hive crud operations

--- birthdaytracker/lib/models/hive_helper.dart
+++ birthdaytracker/lib/models/hive_helper.dart
@@ -26,6 +26,42 @@ class HiveHelper {
     return profiles;
   }
 
+  static void saveProfile(BirthdayProfile profile) {
+    namesBox.put(profile.key, profile.name);
+    bdaysBox.put(profile.key, encodeDate(profile));
+  }
+
+  static void deleteProfile(BirthdayProfile profile) {
+    namesBox.delete(profile.key);
+    bdaysBox.delete(profile.key);
+  }
+
+  static String encodeDate(BirthdayProfile profile) {
+    String days = makeLength("${profile.day}", 2);
+    String months = makeLength("${profile.month}", 2);
+    String years = "0000";
+
+    if (profile.includesYear) {
+      years = makeLength("${profile.year}", 4);
+    }
+
+    return "${days}-${months}-${years}";
+  }
+
+  static String makeLength(String original, int length) {
+    int extra = length - original.length;
+    if (extra < 0) {
+      throw Exception("attempted to extend oversized string '${original}'");
+    }
+
+    String padding = "";
+    for (int i = 0; i < extra; i++) {
+      padding += "0";
+    }
+
+    return padding + original;
+  }
+
   static BirthdayProfile? getProfile(int key) {
     if (namesBox.containsKey(key) && bdaysBox.containsKey(key)) {
       String name = namesBox.get(key);
