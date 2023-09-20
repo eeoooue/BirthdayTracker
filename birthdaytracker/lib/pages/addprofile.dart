b5e89a411883ac215ad19104329828bfb59df15d
eeoooue <pdmoorhouse@gmail.com>
2023-09-20 05:06:58 +0100

    added TimePicker, TextField and MaterialButton inputs to AddProfile page

--- birthdaytracker/lib/pages/addprofile.dart
+++ birthdaytracker/lib/pages/addprofile.dart
@@ -12,7 +12,47 @@ class AddProfile extends StatefulWidget {
 }
 
 class _AddProfileState extends State<AddProfile> {
+  final _textController = TextEditingController();
+
+  bool includeYear = false;
+  DateTime selectedTime = DateTime(2500);
+  String dateButtonString = "Select Date";
+
+  String getFormattedTime() {
+    int month = selectedTime.month;
+    int day = selectedTime.day;
+    BirthdayProfile profile = BirthdayProfile("formatting", month, day);
+
+    return profile.getBirthdayString();
+  }
+
   _AddProfileState();
+
+  bool canSubmit() {
+    if (_textController.text.isEmpty) {
+      return false;
+    }
+    if (selectedTime.year == 2500) {
+      return false;
+    }
+    return true;
+  }
+
+  void submit() {
+    if (!canSubmit()) {
+      return;
+    }
+
+    String name = _textController.text;
+    int month = selectedTime.month;
+    int day = selectedTime.day;
+
+    BirthdayProfile profile = BirthdayProfile(name, month, day);
+
+    if (includeYear) {
+      profile.setYear(selectedTime.year);
+    }
+  }
 
   void _navigateBottomBar(int index) {
     if (Navigator.canPop(context)) {
@@ -31,23 +71,81 @@ class _AddProfileState extends State<AddProfile> {
     }
   }
 
+  void _showDatePicker() {
+    showDatePicker(
+            context: context,
+            initialDate: DateTime(DateTime.now().year),
+            firstDate: DateTime(1950),
+            lastDate: DateTime(2150))
+        .then((value) {
+      setState(() {
+        selectedTime = value!;
+        dateButtonString = getFormattedTime();
+      });
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(
+        title: const Text(
           "Add Profile",
           style: TextStyle(color: Colors.white),
         ),
         backgroundColor: Colors.blue,
       ),
       bottomNavigationBar:
-          BottomNavigationBar(onTap: _navigateBottomBar, items: [
+          BottomNavigationBar(onTap: _navigateBottomBar, items: const [
         BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
         BottomNavigationBarItem(icon: Icon(Icons.people), label: "People"),
         BottomNavigationBarItem(icon: Icon(Icons.settings), label: "Settings"),
       ]),
-      body: Column(),
+      body: Column(
+        children: [
+          Padding(
+            padding: EdgeInsets.all(20),
+            child: TextField(
+              controller: _textController,
+              decoration: const InputDecoration(
+                  hintText: "Name", border: OutlineInputBorder()),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: MaterialButton(
+              onPressed: _showDatePicker,
+              color: Colors.grey[300],
+              child: Text(dateButtonString),
+            ),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: MaterialButton(
+                  onPressed: () {
+                    submit();
+                  },
+                  child: Text("Submit"),
+                  color: Colors.blue,
+                  textColor: Colors.white,
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: MaterialButton(
+                  onPressed: () {},
+                  child: Text("Cancel"),
+                  color: Colors.grey[200],
+                  textColor: Colors.red,
+                ),
+              ),
+            ],
+          )
+        ],
+      ),
     );
   }
 }
